a0ce8a54a0e3dd2211422fb4c74b23014a71ba6f
farotory <myboxlolkek@inbox.ru>
2025-05-18 18:44:24 +0300

    3
    3

--- lab3/importnumpyasnplab3.py
+++ lab3/importnumpyasnplab3.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 def ackley_function(x):
-    
     x = np.array(x)
     D = len(x)
     sum_sq = np.sum(x**2)
@@ -30,12 +29,12 @@ Z = np.zeros_like(X1)
 # Вычисляем значения функции для каждой точки сетки
 for i in range(num_points):
     for j in range(num_points):
-        Z[i,j] = ackley_function([X1[i,j], X2[i,j]])
+        Z[i, j] = ackley_function([X1[i, j], X2[i, j]])
 
 # Значение функции в тестовой точке
 test_point_value = ackley_function([x10, x20])
 
-# Создаем фигуру с 4 подграфиками
+# Создаем фигуру c 4 подграфиками
 fig = plt.figure(figsize=(16, 12))
 fig.suptitle(f'Функция на интервалах x1∈[{x1_min}, {x1_max}], x2∈[{x2_min}, {x2_max}]\n'
              f'Значение в точке ({x10}, {x20}) = {test_point_value:.2f}', fontsize=14)
@@ -63,34 +62,25 @@ ax2.view_init(elev=90, azim=0)  # Вид сверху
 ax2.legend()
 fig.colorbar(surf2, ax=ax2, shrink=0.5, aspect=5)
 
-# 3. График при x1 = x2 (линия через точку (x10, x20))
-t = np.linspace(max(x1_min, x2_min), min(x1_max, x2_max), num_points)
-line_values = np.array([ackley_function([ti, ti]) for ti in t])
-
+# 3. График при x1 = x10 (сечение вдоль x2)
 ax3 = fig.add_subplot(223)
-ax3.plot(t, line_values)
-ax3.scatter(0, test_point_value, color='red', s=100, label=f'x=0, f={test_point_value:.2f}')
-ax3.set_title(f'График при x1 = x2\nИнтервал [{max(x1_min, x2_min):.1f}, {min(x1_max, x2_max):.1f}]')
-ax3.set_xlabel('x (x1 = x2)')
-ax3.set_ylabel('f(x,x)')
+ax3.plot(x2, [ackley_function([x10, x2i]) for x2i in x2])
+ax3.scatter(x20, test_point_value, color='red', s=100, label=f'x2={x20}, f={test_point_value:.2f}')
+ax3.set_title(f'График при x1 = {x10}\nИнтервал x2∈[{x2_min}, {x2_max}]')
+ax3.set_xlabel('x2')
+ax3.set_ylabel(f'f({x10}, x2)')
 ax3.grid(True)
 ax3.legend()
 
-# 4. Контурный график с выделенной точкой (x10, x20)
+# 4. График при x2 = x20 (сечение вдоль x1)
 ax4 = fig.add_subplot(224)
-contour = ax4.contourf(X1, X2, Z, levels=20, cmap='viridis')
-ax4.plot(x10, x20, 'ro', markersize=10, label=f'({x10}, {x20})')
-ax4.set_title(f'Контурный график\nx1∈[{x1_min}, {x1_max}], x2∈[{x2_min}, {x2_max}]')
+ax4.plot(x1, [ackley_function([x1i, x20]) for x1i in x1])
+ax4.scatter(x10, test_point_value, color='red', s=100, label=f'x1={x10}, f={test_point_value:.2f}')
+ax4.set_title(f'График при x2 = {x20}\nИнтервал x1∈[{x1_min}, {x1_max}]')
 ax4.set_xlabel('x1')
-ax4.set_ylabel('x2')
-ax4.annotate(f'({x10}, {x20})\nf={test_point_value:.2f}', 
-             (x10, x20), 
-             textcoords="offset points", 
-             xytext=(10,10), 
-             ha='center',
-             bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5))
+ax4.set_ylabel(f'f(x1, {x20})')
+ax4.grid(True)
 ax4.legend()
-fig.colorbar(contour, ax=ax4, shrink=0.8, aspect=10)
 
 plt.tight_layout()
 plt.show()
